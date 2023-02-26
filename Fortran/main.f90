a9d9d7c63a406585e2bd1939d5ac05a2d4392dc0
MeblIkea <salulespotos@gmail.com>
2023-02-26 15:23:57 +0100

    Fortran is now Good to go

--- Fortran/main.f90
+++ Fortran/main.f90
@@ -30,8 +30,27 @@ contains
 
     subroutine graph(htab)
         real(kind=8), allocatable :: htab(:)
+        integer :: i
 
-        print *, "graph"
+        open(unit=9,file="data.txt",action="write")
+        do i = 1, size(htab)
+            write(9, *) htab(i)
+        end do
+        close(9)
+
+        open(unit=10,file="data_plot.gnu",action="write")
+           write(10, *) 'reset'
+           write(10, *) "set terminal pngcairo dashed enhanced size 480,360 font 'arial,12' fontscale 1.0"
+           write(10, *) "set encoding utf8"
+           write(10, *) "set output 'plot.png'"
+           write(10, *) 'set xlabel "Nombre de rebonds'
+           write(10, *) 'set ylabel "Hauteur"'
+           write(10, *) "set title 'Hauteur de chaques rebonds'"
+           write(10, *) 'plot "./data.txt"'
+        close(10)
+
+        call system("gnuplot data_plot.gnu")
+
     end subroutine graph
 
 end module
@@ -45,5 +64,4 @@ program main
 
     htab = rebonds(b)
     call graph(htab)
-    print *, htab
 end program
